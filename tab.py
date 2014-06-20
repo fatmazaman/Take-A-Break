1edc8ce6546b1ec145014a1d387d643bfd3c9e96
fatmazaman <fatma302@gmail.com>
2014-06-20 22:10:53 +0530

    Program

---
+++ tab.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+import webbrowser
+import time
+
+total_break = 4
+break_count = 0
+
+print("This Program Started On:"+time.ctime())
+
+while(break_count<total_break):
+	time.sleep(10)
+	print("Take A Break")
+	webbrowser.open("http://www.youtube.com/watch?v=AKKT7zE_0Zg")
+	break_count +=1
